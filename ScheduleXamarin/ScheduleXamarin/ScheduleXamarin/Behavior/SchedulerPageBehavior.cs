057b14bd056d138ae80cfd59f45cb7f5284db667
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-18 18:10:06 +0530

    XAMARIN-35832 - Sample and screenshot added.

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulerPageBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulerPageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.Content.FindByName<SfSchedule>("Schedule");
+            this.WireEvents();
+        }
+        private void WireEvents()
+        {
+            this.schedule.VisibleDatesChangedEvent += Schedule_VisibleDatesChangedEvent;
+        }
+
+        private void Schedule_VisibleDatesChangedEvent(object sender, VisibleDatesChangedEventArgs e)
+        {
+            var month = e.visibleDates[e.visibleDates.Count / 2].Date.Month;
+            var year = e.visibleDates[e.visibleDates.Count / 2].Date.Year;
+            App.Current.MainPage.DisplayAlert(month.ToString(), year.ToString(), "Ok");
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.UnWireEvents();
+        }
+        private void UnWireEvents()
+        {
+            this.schedule.VisibleDatesChangedEvent += Schedule_VisibleDatesChangedEvent;
+        }
+    }
+}
+
+
